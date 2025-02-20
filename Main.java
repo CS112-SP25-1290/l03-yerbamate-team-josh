adee8aa3eebe9225757f63d55a7a9739dcde9706
KlowasMunik <78110073+KlowasMunik@users.noreply.github.com>
2025-02-20 20:04:19 +0000

    n

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args)
-  	{
+	{
 		CaffeinatedBeverage[] inventory = new CaffeinatedBeverage[10];
 		Scanner keyboard = new Scanner(System.in);
 		// Prompt user with options in a loop
@@ -12,7 +12,7 @@ public class Main
 		double price;
 		
 		do
-    	{
+		{
 			System.out.println("1) Enter new Tea");
 			System.out.println("2) Enter new Yerba Mate");
 			System.out.println("3) Exit");
@@ -22,21 +22,21 @@ public class Main
 			switch (choice)
 			{
 			    case 1: // Tea
-			        System.out.print("Enter name      : ");
-			        name = keyboard.nextLine();
-			        System.out.print("Enter ounces    : ");
-			        ounces = keyboard.nextInt();
-			        System.out.print("Enter price     $ ");
-			        price = keyboard.nextDouble();
-			        System.out.print("Enter brew temperature (in Celsius): ");
-			        brewTemp = keyboard.nextInt();
+					System.out.print("Enter name      : ");
+					name = keyboard.nextLine();
+					System.out.print("Enter ounces    : ");
+					ounces = keyboard.nextInt();
+					System.out.print("Enter price     $ ");
+					price = keyboard.nextDouble();
+					System.out.print("Enter brew temperature (in Celsius): ");
+					brewTemp = keyboard.nextInt();
 			
 			        // Create a Tea, put into array
-			        inventory[count] = new Tea(name, ounces, price, brewTemp);
+					inventory[count] = new Tea(name, ounces, price, brewTemp);
 
 					System.out.println("Your tea order has been added: " + inventory[count]);
-			        count++;
-			        break;
+					count++;
+					break;
 			
 			}
 
